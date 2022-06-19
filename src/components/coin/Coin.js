99800f3b38abd630bc46775e797c3b3c64b2cf77
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-06-18 20:23:56 -0400

    updated jsx

--- src/components/coin/Coin.js
+++ src/components/coin/Coin.js
@@ -5,20 +5,20 @@ function Coin({name,image,symbol,price,volume, priceChange, marketCap}) {
 
   console.log(`this is volume ${volume}`)
   return (
-  <div data-testid='coin col-md'>
-    <div className="coin-container">
-      <div className="coin-row">
-        <div className="coin">
-          <img src={image} alt="crypto" />
+  <div data-testid='coin1 col-md col-sm col-xs'>
+    <div className="coin-container col-md col-sm col-xs">
+      <div className="coin-row col-md col-sm col-xs">
+        <div className="coin col-md col-sm col-xs">
+          <img className="col-md col-sm col-xs .img-fluid" src={image} alt="crypto" />
           <h1>{name}</h1>
-          <p className="coin-symbol"></p>
+          <p className="coin-symbol col-md col-sm col-xs"></p>
         </div>
-        <div className="coin-data">
-          <p className="coin-price"> ${price}</p>
-          <p className="coin-volume"> ${volume}</p>
-          {priceChange < 0 ? (<p className='coin-percent red'>{priceChange}</p>): (<p>{priceChange}</p>)}
+        <div className="coin-data col-md col-xs">
+          <p className="coin-price col-md col-xs"> ${price}</p>
+          <p className="coin-volume col-md col-xs"> ${volume}</p>
+          {priceChange < 0 ? (<p className='coin-percent red col-md col-xs'>{priceChange}</p>): (<p>{priceChange}</p>)}
           
-          <p className='coin-marketcap'>
+          <p className='coin-marketcap col-md col-xs'>
               mkt Cap: ${marketCap}
               
           </p>
